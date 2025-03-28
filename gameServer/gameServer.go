2a52c7d9a2963d9135d9d46775642814a20eb8d3
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-03-28 20:35:12 +0100

    refactor(gameServer)
    Added assertions.
    Split some functions.

--- gameServer/gameServer.go
+++ gameServer/gameServer.go
@@ -31,57 +31,53 @@ func InitGameServer() *Server {
 		matcher: make(chan uuid.UUID, 2),
 	}
 	srv.sync = CreateSynchronizer(srv)
+
 	go srv.matchMaker()
 
 	return srv
 }
 
-func (srv *Server) AddConnection(conn *connection.Connection) error {
-	srv.mut.Lock()
-	defer srv.mut.Unlock()
+func (srv *Server) AddConnection(conn *connection.Connection) {
+	assert.NotNil(conn, "connection was nil")
+	assert.NotNil(srv.sync, "gameServer sync was nil")
 
-	id, err := uuid.NewUUID()
-	if err != nil {
-		return errors.New("cannot generate uuid for this connection")
-	}
-
+	id := srv.generateUUID()
 	pConn := CreatePlayerConnection(srv.sync, id, conn)
 
-	srv.connections[id] = pConn
-	srv.matcher <- id
-
-	log.Printf("connected to %q, uuid:%q\n", conn.GetRemoteIP(), id.String())
+	srv.addPlayerConnection(id, pConn)
 
 	conn.StartReceiving()
 	pConn.StartLoop()
 
-	return nil
+	log.Printf("connected to %q, uuid:%q\n", conn.GetRemoteIP(), id.String())
 }
 
 func (srv *Server) DeleteConnection(id uuid.UUID) {
-	srv.mut.Lock()
-	defer srv.mut.Unlock()
+	conn, err := srv.getConnection(id)
+	assert.NoError(err, "player connection does not exist")
 
-	srv.connections[id].EndLoop()
+	conn.EndLoop()
 
-	delete(srv.connections, id)
+	srv.removeConnection(id)
 }
 
 func (srv *Server) HandleConnection(w http.ResponseWriter, r *http.Request) error {
     socket, err := upgrader.Upgrade(w, r, nil)
-	
+	defer r.Body.Close() 
+
     if err != nil {
-		r.Body.Close() // Is it needed?
         return err
     }
 
 	conn := connection.CreateConnection(socket)
-	err = srv.AddConnection(conn)
+	srv.AddConnection(conn)
 
-	return err
+	return nil
 }
 
 func (srv *Server) Update() {
+	assert.NotNil(srv.sync, "server sync was nil")
+
 	srv.updateAllRooms()
 	srv.sync.SyncTransferAll()
 }
@@ -93,6 +89,8 @@ func (srv *Server) updateAllRooms() {
 }
 
 func (srv *Server) addRoom(room *handlers.Room) {
+	assert.NotNil(room, "room was nil")
+
 	srv.mut.Lock()
 	defer srv.mut.Unlock()
 
@@ -100,13 +98,14 @@ func (srv *Server) addRoom(room *handlers.Room) {
 }
 
 func (srv *Server) createRoom(pConnections [2]*PlayerConnection) *handlers.Room {
+	assert.NotNil(srv.sync, "server synchronizer was nil")
+
 	log.Println("creating room")
 
-	uuid, err := uuid.NewUUID()
-	assert.NoError(err, "This should never return an error.")
-
+	uuid := srv.generateUUID()
 	room := CreateRoom(srv.sync, pConnections, uuid)
 
+	assert.NotNil(room, "room was nil")
 	return room
 }
 
@@ -118,21 +117,20 @@ func (srv *Server) removeRoom(roomUUID uuid.UUID) {
 }
 
 func (srv *Server) sendMessage(connId uuid.UUID, msg *message.Message) {
-	srv.mut.Lock()
-	defer srv.mut.Unlock()
+	assert.NotNil(msg, "message was nil")
+	
+	pConn, err := srv.getConnection(connId)
+	assert.NoError(err, "player connection was nil")
 
-	conn := srv.connections[connId].GetConnection()
-	// TODO: Check this error!
+	conn := pConn.GetConnection()
 	conn.SendMessage(msg)
 }
 
 // Blocking
 func (srv *Server) matchMaker() {
 	for {
-		id1 := <-srv.matcher
-		id2 := <-srv.matcher
-		c1 := srv.getConnection(id1)
-		c2 := srv.getConnection(id2)
+		id1, c1 := srv.getConnectionFromMatcher()
+		id2, c2 := srv.getConnectionFromMatcher()
 
 		a1 := c1.GetConnection().SendPing() == nil
 		a2 := c2.GetConnection().SendPing() == nil
@@ -151,6 +149,14 @@ func (srv *Server) matchMaker() {
 	}
 }
 
+func (srv *Server) getConnectionFromMatcher() (uuid.UUID, *PlayerConnection) {
+	id := <-srv.matcher
+	conn, err := srv.getConnection(id)
+
+	assert.NoError(err, "player connection was nil")
+	return id, conn
+}
+
 func (srv *Server) eventNotHandled(e event.Event) {
 	// TODO: Check if it's logged correctly.
 	log.Printf("event not handled: %+v", e)
@@ -161,15 +167,18 @@ func (srv *Server) Handle(e event.Event) {
 
 	switch eType {
 	case event.EventTypeSendMessage:
-		eSendMessage, _ := e.(EventSendMessage)
+		eSendMessage, ok := e.(EventSendMessage)
+		assert.Assert(ok, "type assertion failed for event sendMessage")
 
 		srv.sendMessage(eSendMessage.ConnectionId, &eSendMessage.Msg)
 		
 	case event.EventTypeExit:
-		eExit, _ := e.(EventExit)
+		eExit, ok := e.(EventExit)
+		assert.Assert(ok, "type assertion failed for event exit")
 
 		srv.matcher <- eExit.OpponentConnId
 		srv.DeleteConnection(eExit.ConnectionId)
+
 		log.Println("removing room")
 		srv.removeRoom(eExit.RoomUUID)
 
@@ -178,17 +187,47 @@ func (srv *Server) Handle(e event.Event) {
 	}
 }
 
-func (srv *Server) getConnection(id uuid.UUID) *PlayerConnection {
-	srv.mut.Lock()
-	defer srv.mut.Unlock()
-
-	return srv.connections[id]
-}
-
 var upgrader = websocket.Upgrader {
 	ReadBufferSize:  2048,
 	WriteBufferSize: 2048,
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
+}
+
+func (srv *Server) generateUUID() uuid.UUID {
+	uuid, err := uuid.NewUUID()
+
+	assert.NoError(err, "uuid generation error")
+	return uuid
+}
+
+func (srv *Server) addPlayerConnection(uuid uuid.UUID, pConn *PlayerConnection) {
+	assert.NotNil(pConn, "player connection was nil")
+
+	srv.mut.Lock()
+	defer srv.mut.Unlock()
+
+	srv.connections[uuid] = pConn
+	srv.matcher <- uuid
+}
+
+func (srv *Server) removeConnection(uuid uuid.UUID) {
+	srv.mut.Lock()
+	defer srv.mut.Unlock()
+
+	delete(srv.connections, uuid)
+}
+
+func (srv *Server) getConnection(id uuid.UUID) (*PlayerConnection, error) {
+	srv.mut.Lock()
+	defer srv.mut.Unlock()
+
+	conn, ok := srv.connections[id]
+
+	if !ok {
+		return nil, errors.New("connection does not exist")
+	}
+
+	return conn, nil
 }
