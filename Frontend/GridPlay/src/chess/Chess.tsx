27f3e1d36359c0fb770f4e491bd6067b5dbd0030
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-04-12 14:28:57 +0200

    refactor(front-end): Replace example files with project.

---
+++ Frontend/GridPlay/src/chess/Chess.tsx
@@ -0,0 +1,12 @@
+import './Chess.css'
+import { Chessboard } from 'react-chessboard';
+
+function Chess() {
+    return (
+    <div>
+        <Chessboard id="BasicBoard" />
+    </div>
+    );
+}
+
+export default Chess;
