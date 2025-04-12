856d98523affd6ba8a30c22b4184f38e54dc49c5
Mateusz Stulczewski <mstulczewski@gmail.com>
2025-04-12 12:50:48 +0200

    test(ChessGame): Update game test.

--- Backend/game/chessGame/game_test.go
+++ Backend/game/chessGame/game_test.go
@@ -114,8 +114,8 @@ func TestGetGameStartMessage(t *testing.T) {
 	chessGame := CreateChessGame()
 
 	msg := chessGame.GetGameStartMessage(0)
-	assert.Equal(t, "white", msg.Data.(msgGameStart).color, "Expected color to be white for player 0")
+	assert.Equal(t, "white", msg.Data.(msgGameStart).Color, "Expected color to be white for player 0")
 
 	msg = chessGame.GetGameStartMessage(1)
-	assert.Equal(t, "black", msg.Data.(msgGameStart).color, "Expected color to be black for player 1")
+	assert.Equal(t, "black", msg.Data.(msgGameStart).Color, "Expected color to be black for player 1")
 }
